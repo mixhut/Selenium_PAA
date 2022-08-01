ce32b1bc8a67670e8b4884e8721e9b9bbb44dda5
mixhut <domains.vorlage@8alias.com>
2022-08-01 01:32:30 +0100

    v1.7 See README

--- functions/tools.py
+++ functions/tools.py
@@ -1,22 +1,102 @@
 import pickle
+import json
+import os
+# import sys
 from os.path import exists
 
 
 def save_pickle(name, data):
-    with open(name, 'wb') as f:
-        pickle.dump(data, f)
+	with open(name, 'wb') as f:
+		pickle.dump(data, f)
 
 
 def load_pickle(name):
-    with open(name, 'rb') as f:
-        return pickle.load(f)
+	with open(name, 'rb') as f:
+		return pickle.load(f)
 
 
-# def checkpoint_check(CHECKPOINT_FILE):
-#     checkpoint_exists = exists(checkpoint_file)
-#     if checkpoint_exists is True:
-#         with open(CHECKPOINT_FILE, 'r') as f:
-#             checkpoint = int(f.readline())
-#         print('Checkpoint found:', checkpoint)
-#     else:
-#         checkpoint = int(0)
+def pickle_it(file, new_data):
+	# Check if pickle file already exists, if not make new file
+	pickle_exists = exists(file)
+	if pickle_exists is True:
+		data = load_pickle(file)
+		data.append(new_data)
+		save_pickle(file, data)
+	else:
+		save_pickle(file, new_data)
+
+
+def pickle2json(file):
+	# open pickle file
+	with open(file, 'rb') as file_in:
+		obj = pickle.load(file_in)
+	# convert pickle object to json object
+	json_obj = json.loads(json.dumps(obj, default=str))
+	# write the json file
+	with open(os.path.splitext(file)[0] + '.json', 'w', encoding='utf-8') as file_out:
+		json.dump(json_obj, file_out, ensure_ascii=False, indent=4)
+
+
+def cp_load(file):
+	checkpoint_exists = exists(file)
+	if checkpoint_exists is True:
+		with open(file, 'r', encoding='utf-8') as f:
+			check = int(f.readline())
+			print('Checkpoint found:', check)
+			return check
+	else:
+		check = int(0)
+		return check
+
+
+def cp_write(file, cp: int):
+	with open(file, 'w', encoding='utf-8') as f:
+		f.write(str(cp))
+		f.close()
+
+
+def save_json(file, data):
+	with open(file, 'a', encoding='utf-8') as f:  # appends to json file
+		json.dump(data, f, indent=4)
+		f.write(',\n')
+
+
+def load_json(file):
+	with open(file, 'r', encoding='utf-8') as f:
+		data = json.load(f)
+	return data
+
+
+def remove_duplicate_keys(j_data, j_key):
+	unique_elements = []
+	cleaned_data = []
+	keys = []
+	for x, j in enumerate(j_data):
+		if j_data[x][j_key] not in unique_elements:
+			unique_elements.append(j_data[x][j_key])
+			keys.append(x)
+
+	for key in keys:
+		cleaned_data.append(j_data[key])
+
+	return cleaned_data
+
+
+# Append JSON object to output file JSON array
+def append_json(file, data):
+	# Check exists
+	json_exists = exists(file)
+	if json_exists is True:
+		with open(file, 'a+', encoding='utf-8') as outfile:
+			outfile.seek(0, os.SEEK_END)
+			outfile.seek(outfile.tell()-1, os.SEEK_SET)
+			outfile.truncate()
+			outfile.write(',')
+			json.dump(data, outfile, indent=4)
+			outfile.write('\n]')
+	else:
+		# Create file
+		with open(file, 'w', encoding='utf-8') as f:  # appends to json file
+			f.write('[\n')
+			json.dump(data, f, indent=4)
+			f.write('\n')
--- main.py
+++ main.py
@@ -1,37 +1,23 @@
-# # # # Selenium_PAA - v1.5 # # # # #
+# # # # Selenium_PAA - v1.7 # # # # #
 
 from functions.foxwd_new import GSearch as GSearch
-from functions.tools import load_pickle, save_pickle
-from constants import PROJECT_DIR, PICKLE_FILE, CHECKPOINT_FILE, PROJECT_NAME
+from functions.tools import pickle_it, pickle2json, cp_load, cp_write
+from functions.tools import load_json, append_json
+from functions.constants import project_root, PICKLE_FILE, CHECKPOINT_FILE, project_name, JSON_FILE
 from os.path import exists
 
 # # # # # --> SET VARIABLES <-- # # # # # #
-# PROJECT_NAME = 'FreezeDried' < Constants.py (until CLI integration)
-input_questions = PROJECT_DIR + '/input/' + PROJECT_NAME + '-Questions.txt'
-checkpoint = 0  # placeholder
+# project_name = 'NameOfProject' < Constants.py (until CLI integration)
+input_questions = project_root + '/input/' + project_name + '-Questions.txt'
 # # # # # # # # # # # # # # # # # # # # # #
 
-print(input_questions)
-# input_keys = PROJECT_DIR + '/input/test.csv'
-print(CHECKPOINT_FILE)
+# input_keys = project_root + '/input/test.csv'
 
-# Check Pickle exist & load
-pickle_exists = exists(PICKLE_FILE)
-if pickle_exists is True:
-    data = load_pickle(PICKLE_FILE)
-else:
-    data = []
 
-# Check for error checkpoint
-checkpoint_exists = exists(CHECKPOINT_FILE)
-if checkpoint_exists is True:
-    with open(CHECKPOINT_FILE, 'r') as f:
-        checkpoint = int(f.readline())
-    print('Checkpoint found:', checkpoint)
-else:
-    checkpoint = int(0)
+# Look for error checkpoint
+checkpoint = cp_load(CHECKPOINT_FILE)
 
-with open(input_questions, 'r') as file:
+with open(input_questions, 'r', encoding='UTF-8') as file:
     my_questions = [line.rstrip('\n') for line in file]
 
 print('=== Total lines to be processed:', len(my_questions), '===')
@@ -40,21 +26,37 @@ for i in range(checkpoint, len(my_questions)):
     print(' ~ Getting Answer', i)
     print(' ~~ Question:', my_questions[i])
     try:
-        gs = GSearch(my_questions[i]).get_answer()
-        output = gs
-        print(output)
-        data.append(output)
-        save_pickle(PICKLE_FILE, data)
+        data = []
+        gs = GSearch(my_questions[i], 7).get_answer()
+        data.append(gs)
+        append_json(JSON_FILE, data)
+        # pickle_it(PICKLE_FILE, data)
+        # Clear table
+        data.clear()  # Clear datatable
+        cp_write(CHECKPOINT_FILE, i)
     except Exception as e:
         print(' -- Skipping Answer... -', my_questions[i])
         i = i + 1
-        f = open(CHECKPOINT_FILE, 'w')
-        f.write(str(i))
-        f.close()
-    else:
-        f = open(CHECKPOINT_FILE, 'w')
-        f.write(str(i))
-        f.close()
+        cp_write(CHECKPOINT_FILE, i)
+
+
+# # Convert pickle to JSON
+# if len(my_questions) == checkpoint:
+#     pickle2json(PICKLE_FILE)
+# else:
+#     print('No Pickle > JSON conversion - job incomplete.')
 
 
 print('=== Completed ===')
+
+# # # # # # Stage 3 # # # # # #
+# Take related output from Stage 2 and convert into simple JSON with Q & As.
+
+
+# NOTES/TO DO:
+# Switch pickle back to JSON + remove conversion
+# Added length to the GSearch class - might have to update other files if inconsistent
+# Remove clearing of datatable every loop? Not necessary after isolating crash issue to selenium tmp files
+# Likely have to change JSON defined functions once structured JSON data better.
+#
+# Make next stages into Class/Functions then append to this script
